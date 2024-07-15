c5f37eeffc0232488c30ec8a113228bd0dffa195
Ofentse Sithole <ST10104487@rcconnect.edu.za>
2024-07-15 13:00:43 +0200

    update

--- HRHarmonySystem/Form1.Designer.cs
+++ HRHarmonySystem/Form1.Designer.cs
@@ -196,7 +196,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(450, 429);
+            this.label1.Location = new System.Drawing.Point(439, 429);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(149, 16);
             this.label1.TabIndex = 9;
